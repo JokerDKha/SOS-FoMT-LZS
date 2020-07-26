934845794dcdb5eac68bbfa589f6dd74ba0177e2
JokerD <56380626+JokerDKha@users.noreply.github.com>
2020-07-26 10:37:03 +0700

    Add files via upload

---
+++ extract.bat
@@ -0,0 +1,2 @@
+quickbms.exe "%cd%\sosfomt_lzs.bms" "%cd%\lzs-here" "%cd%\output"
+quickbms.exe "%cd%\sosfomt_dcmp.bms" "%cd%\output" "%cd%\File-extract"
---
+++ pack.bat
@@ -0,0 +1,2 @@
+quickbms.exe -G -w -r -r "%cd%\sosfomt_dcmp.bms" "%cd%\output" "%cd%\File-extract"
+quickbms.exe -G -w -r -r -r "%cd%\sosfomt_lzs.bms" "%cd%\lzs-here" "%cd%\output"
